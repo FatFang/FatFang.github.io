a31bbff85fb9877dd3918d36990d4afcca541d6f
FatFang <gbossfamily2@gmail.com>
2022-04-13 00:25:13 +0800

    mouse

--- playList.js
+++ playList.js
@@ -1,12 +1,12 @@
 //YouTube Video ID
 let playList = [
-    "SwZMS-ISbH4",
+    "aHdKcQTsUzM",
     "HXf4Ltggjfw",
     "c6gi6IP6ay4"
 ];
 //播放起訖秒數
 let playTime = [
-    [101, 109],
+    [0, 109],
     [144, 150],
     [146, 152]
 ];
